1fe7a0017c9a36ee5c8b4f48b301ec7e2e8eb860
unknown <brw1820@gmail.com>
2020-06-11 21:52:36 -0400

    updates to homework

--- script.js
+++ script.js
@@ -16,31 +16,31 @@
 
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
-var startButton = document.getElementById("startButton")
-var timer = document.getElementById("remaining-time")
-var jumbotron = document.getElementById("jumbotron")
+var startButton = document.getElementById("startButton");
+var timer = document.getElementById("remaining-time");
+var jumbotron = document.getElementById("jumbotron");
 
 var questionsArray =  [{
   question: "Commonly used data types DO NOT include",
   Answers: ["strings", "booleans", "strings", "alerts"],
-  rightAnswer: 3,
+  rightAnswer: "strings",
 },
 {
   question: "The condition in an if/else statement is enclosed within:",
   firstAnswer: ["quotes", "curly brackets", "parentheses", "square brackets"],
-  rightAnswer: 2,
+  rightAnswer1: "curly brackets",
 }, {
   question: "Arrays in Javscript can be use to store:",
   firstAnswer: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-  rightAnswer: 4,
+  rightAnswer2: "all of the above",
 }, {
   question: "String values must be enclosed within ______ when being assigned to variables.",
   firstAnswer: ["commas", "curly brackets", "quotes", "parentheses"],
-  rightAnswer: 3,
+  rightAnswer3: "quotes",
 },  {
   question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-  Answers: ["Javascript", "terminal/bash", "for loops", "console log"],
-  rightAnswer: 4,
+  fourthAnswers: ["Javascript", "terminal/bash", "for loops", "console log"],
+  rightAnswere4: "console log",
 }]
 
 var currentQuestionIndex = 0
@@ -54,7 +54,7 @@ function setTime() {
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
-      sendMessage();
+      sendMessage("You are out of time!");
     }
 
   }, 1000);
@@ -69,19 +69,21 @@ setTime();
 //   mainEl.appendChild(imgEl);
 function displayQuestions() {
     
-var currentQuestion = questionsArray[currentQuestionIndex];
+var questionDisplayed = questionsArray[currentQuestionIndex];
 jumbotron.textContent = "";
-startButton.remove();
-var hEl=document.createElement("h");
+
+var h4El=document.createElement("h4");
+h4El.textContent = questionDisplayed.question;
+jumbotron.append(h4El);
 
 
 // document.getElementsById("quiz-screen").innerhtml="";
 // document.getElementById("startButton").addEventListener("click", function(){})
 }
-
+displayQuestions();
 function nextQ()    {
 
 }
 
-startButton.addEventListener("click", nextQ)
-startButton.addEventListener("click", setTime)
+startButton.addEventListener("click", displayQuestions);
+startButton.addEventListener("click", setTime); 
